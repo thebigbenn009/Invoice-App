2962e025cb4575998a17157dc2c3da1baee45f2c
Benedict <aliubenedict@gmail.com>
2023-10-28 22:47:35 +0100

    invoice-card

--- src/components/InvoiceCard.jsx
+++ src/components/InvoiceCard.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const InvoiceCard = () => {
-  return <div>InvoiceCard</div>;
+  return (
+    <article className="invoice-card">
+      <p className="invoice-text-bold">#RT3080</p>
+      <p className="invoice-text-light">Due 19 August 2023</p>
+      <p className="invoice-text-light name">Jensen Huang </p>
+      <p className="invoice-text-bold">£ 1,800.90</p>
+      <div className="invoice-status-container">
+        <span className="dot"></span>
+        <span className="invoice-status">paid</span>
+      </div>
+      <span>
+        <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M1 1l4 4-4 4"
+            stroke="#7C5DFA"
+            stroke-width="2"
+            fill="none"
+            fill-rule="evenodd"
+          />
+        </svg>
+      </span>
+    </article>
+  );
 };
 
 export default InvoiceCard;
