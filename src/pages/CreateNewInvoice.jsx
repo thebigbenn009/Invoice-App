39fb30c7b0c8a8984904e428d4f3bb094a520fa1
Benedict <aliubenedict@gmail.com>
2023-11-01 23:14:49 +0100

    implemented new invoice

--- src/pages/CreateNewInvoice.jsx
+++ src/pages/CreateNewInvoice.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import InvoiceForm from "../components/InvoiceForm";
 
 const CreateNewInvoice = () => {
-  return <main className="main-section">CreateNewInvoice</main>;
+  return (
+    <main className="invoice-section">
+      <section className="new-invoice">
+        <InvoiceForm />
+      </section>
+    </main>
+  );
 };
 
 export default CreateNewInvoice;
