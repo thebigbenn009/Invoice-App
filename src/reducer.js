1b6a6cd5c165335a89a73d5485e2b249fa310360
Benedict <aliubenedict@gmail.com>
2023-11-02 21:47:25 +0100

    added responsive design

---
+++ src/reducer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const formData = {
+  billerAddress: "",
+  billerCity: "",
+  billerPostCode: "",
+  billerCountry: "",
+  clientName: "",
+  clientEmail: "",
+  clientAddress: "",
+  clientCity: "",
+  clientPostCode: "",
+  clientCountry: "",
+  invoiceDate: "",
+  paymentTerms: "",
+  projectDescription: "",
+};
+// const reducer = (state, action) => {};
